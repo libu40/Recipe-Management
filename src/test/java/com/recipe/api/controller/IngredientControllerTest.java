2b3643684b2fe2574a66b2bb238c5ebd84ea451f
Libu Mathew <libu.mathew@ing.com>
2023-07-26 01:43:38 +0200

    IT update

--- src/test/java/com/recipe/api/controller/IngredientControllerTest.java
+++ src/test/java/com/recipe/api/controller/IngredientControllerTest.java
@@ -1,10 +1,5 @@
 package com.recipe.api.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import com.recipe.api.model.dto.IngredientDto;
@@ -22,6 +17,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * This integration tests the {@link IngredientController} to test whether all the endpoints are
@@ -31,147 +30,90 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 class IngredientControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Autowired private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-  private static Faker faker;
+    private static Faker faker;
 
-  @BeforeAll
-  public static void setUp() {
-    faker = new Faker();
-  }
-
-  @Test
-  void shouldFetchAllIngredients() throws Exception {
-    String fetchedIngredients =
-        mockMvc
-            .perform(get("/api/ingredients").accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    List<IngredientDto> ingredients =
-        List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
-    Assertions.assertNotNull(ingredients);
-    Assertions.assertEquals("Flour", ingredients.get(0).getName(), "names are not equal");
-    Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getRecipes());
-  }
-
-  @Test
-  void shouldFetchIngredientById() throws Exception {
-    String fetchedIngredient =
-        mockMvc
-            .perform(get("/api/ingredients/2").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    IngredientDto ingredient = objectMapper.readValue(fetchedIngredient, IngredientDto.class);
-    Assertions.assertNotNull(ingredient);
-    Assertions.assertEquals("Baking powder", ingredient.getName(), "names are not equal");
-    Assertions.assertNotNull(ingredient.getCreatedAt());
-    Assertions.assertNotNull(ingredient.getUpdatedAt());
-    Assertions.assertNotNull(ingredient.getRecipes());
-  }
-
-  @Test
-  void shouldNotFetchIngredientByInvalidId() throws Exception {
-    String fetchedIngredients =
-        mockMvc
-            .perform(get("/api/ingredients/100").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    IngredientDto ingredient = objectMapper.readValue(fetchedIngredients, IngredientDto.class);
-    Assertions.assertNull(ingredient.getName());
-    Assertions.assertNull(ingredient.getRecipes());
-    Assertions.assertNull(ingredient.getCreatedAt());
-    Assertions.assertNull(ingredient.getUpdatedAt());
-  }
-
-  @Test
-  void shouldCreateIngredient() throws Exception {
-    RecipeDto recipeDto = new RecipeDto();
-    recipeDto.setName(faker.food().dish());
-    recipeDto.setInstruction("IT instruction");
-    recipeDto.setVariant("IT variant");
-    recipeDto.setCreatedAt(LocalDateTime.now());
-    recipeDto.setUpdatedAt(LocalDateTime.now());
-    Set<RecipeDto> recipeSet = new HashSet<>(Collections.singleton(recipeDto));
-    mockMvc
-        .perform(
-            post("/api/ingredients")
-                .content(
-                    asJsonString(
-                        new IngredientDto(
-                            faker.food().ingredient(),
-                            recipeSet,
-                            LocalDateTime.now(),
-                            LocalDateTime.now())))
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andDo(print())
-        .andReturn()
-        .getResponse()
-        .getContentAsString();
-  }
-
-  @Test
-  void shouldFetchIngredientsBySortedAndPaginatedWithDefaultValues() throws Exception {
-    String fetchedIngredients =
-        mockMvc
-            .perform(get("/api/ingredients/page").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    List<IngredientDto> ingredients =
-        List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
-    Assertions.assertNotNull(ingredients);
-    Assertions.assertEquals("Flour", ingredients.get(0).getName(), "names are not equal");
-    Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getRecipes());
-  }
-
-  @Test
-  void shouldFetchIngredientsBySortedAndPaginatedWithSpecifiedValues() throws Exception {
-    String fetchedIngredients =
-        mockMvc
-            .perform(
-                get("/api/ingredients/page")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .param("pageNo", "0")
-                    .param("pageSize", "5")
-                    .param("sortBy", "DESC")
-                    .param("attribute", "name"))
-            .andExpect(status().isOk())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    List<IngredientDto> ingredients =
-        List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
-    Assertions.assertNotNull(ingredients);
-    Assertions.assertEquals("Sugar", ingredients.get(0).getName(), "names are not equal");
-    Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
-    Assertions.assertNotNull(ingredients.get(0).getRecipes());
-  }
-
-  private String asJsonString(final Object obj) {
-    try {
-      return new ObjectMapper().writeValueAsString(obj);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    @BeforeAll
+    public static void setUp() {
+        faker = new Faker();
     }
-  }
+
+    @Test
+    void shouldFetchAllIngredients() throws Exception {
+        String fetchedIngredients = mockMvc.perform(get("/api/ingredients").accept(MediaType.APPLICATION_JSON)).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<IngredientDto> ingredients = List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
+        Assertions.assertNotNull(ingredients);
+        Assertions.assertEquals("Flour", ingredients.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getRecipes());
+    }
+
+    @Test
+    void shouldFetchIngredientById() throws Exception {
+        String fetchedIngredient = mockMvc.perform(get("/api/ingredients/2").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        IngredientDto ingredient = objectMapper.readValue(fetchedIngredient, IngredientDto.class);
+        Assertions.assertNotNull(ingredient);
+        Assertions.assertEquals("Baking powder", ingredient.getName(), "names are not equal");
+        Assertions.assertNotNull(ingredient.getCreatedAt());
+        Assertions.assertNotNull(ingredient.getUpdatedAt());
+        Assertions.assertNotNull(ingredient.getRecipes());
+    }
+
+    @Test
+    void shouldNotFetchIngredientByInvalidId() throws Exception {
+        String fetchedIngredients = mockMvc.perform(get("/api/ingredients/100").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound()).andDo(print()).andReturn().getResponse().getContentAsString();
+        IngredientDto ingredient = objectMapper.readValue(fetchedIngredients, IngredientDto.class);
+        Assertions.assertNull(ingredient.getName());
+        Assertions.assertNull(ingredient.getRecipes());
+        Assertions.assertNull(ingredient.getCreatedAt());
+        Assertions.assertNull(ingredient.getUpdatedAt());
+    }
+
+    @Test
+    void shouldCreateIngredient() throws Exception {
+        RecipeDto recipeDto = new RecipeDto();
+        recipeDto.setName(faker.food().dish());
+        recipeDto.setInstruction("IT instruction");
+        recipeDto.setVariant("IT variant");
+        recipeDto.setCreatedAt(LocalDateTime.now());
+        recipeDto.setUpdatedAt(LocalDateTime.now());
+        Set<RecipeDto> recipeSet = new HashSet<>(Collections.singleton(recipeDto));
+        mockMvc.perform(post("/api/ingredients").content(asJsonString(new IngredientDto(faker.food().ingredient(), recipeSet, LocalDateTime.now(), LocalDateTime.now()))).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(print()).andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void shouldFetchIngredientsBySortedAndPaginatedWithDefaultValues() throws Exception {
+        String fetchedIngredients = mockMvc.perform(get("/api/ingredients/page").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<IngredientDto> ingredients = List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
+        Assertions.assertNotNull(ingredients);
+        Assertions.assertEquals("Flour", ingredients.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getRecipes());
+    }
+
+    @Test
+    void shouldFetchIngredientsBySortedAndPaginatedWithSpecifiedValues() throws Exception {
+        String fetchedIngredients = mockMvc.perform(get("/api/ingredients/page").accept(MediaType.APPLICATION_JSON).param("pageNo", "0").param("pageSize", "5").param("sortBy", "DESC").param("attribute", "name")).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<IngredientDto> ingredients = List.of(objectMapper.readValue(fetchedIngredients, IngredientDto[].class));
+        Assertions.assertNotNull(ingredients);
+        Assertions.assertEquals("Sugar", ingredients.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(ingredients.get(0).getCreatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getUpdatedAt());
+        Assertions.assertNotNull(ingredients.get(0).getRecipes());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/recipe/api/controller/RecipeControllerTest.java
+++ src/test/java/com/recipe/api/controller/RecipeControllerTest.java
@@ -1,13 +1,20 @@
 package com.recipe.api.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import com.recipe.api.enums.RequestDataOptionType;
+import com.recipe.api.enums.RequestKeyType;
+import com.recipe.api.enums.RequestOperationType;
+import com.recipe.api.model.dto.IngredientDto;
 import com.recipe.api.model.dto.RecipeDto;
+import com.recipe.api.model.valueobject.SearchCriteria;
+import com.recipe.api.model.valueobject.SearchRequest;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,6 +23,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * This integration tests the {@link RecipeController} to test whether all the endpoints are
@@ -25,82 +39,146 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 class RecipeControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Autowired private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-  private static Faker faker;
+    private static Faker faker;
 
-  @BeforeAll
-  public static void setUp() {
-    faker = new Faker();
-  }
-
-  @Test
-  void shouldFetchAllRecipes() throws Exception {
-    String fetchedRecipes =
-        mockMvc
-            .perform(get("/api/recipes").accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    List<RecipeDto> recipes = List.of(objectMapper.readValue(fetchedRecipes, RecipeDto[].class));
-    Assertions.assertNotNull(recipes);
-    Assertions.assertEquals(
-        "Peanut Butter Pretzel Cookies", recipes.get(0).getName(), "names are not equal");
-    Assertions.assertEquals("Test", recipes.get(0).getInstruction(), "instructions are not equal");
-    Assertions.assertEquals("Vegetarian", recipes.get(0).getVariant(), "variants are not equal");
-    Assertions.assertEquals(4, recipes.get(0).getServingCount(), "serving count are not equal");
-    Assertions.assertNotNull(recipes.get(0).getCreatedAt());
-    Assertions.assertNotNull(recipes.get(0).getUpdatedAt());
-    Assertions.assertNotNull(recipes.get(0).getIngredients());
-  }
-
-  @Test
-  void shouldFetchRecipeById() throws Exception {
-    String fetchedRecipe =
-        mockMvc
-            .perform(get("/api/recipes/2").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
-    Assertions.assertNotNull(recipe);
-    Assertions.assertEquals(
-        "Green Beans with Toasted Butter Pecans", recipe.getName(), "names are not equal");
-    Assertions.assertNotNull(recipe.getCreatedAt());
-    Assertions.assertNotNull(recipe.getVariant());
-    Assertions.assertNotNull(recipe.getInstruction());
-    Assertions.assertNotNull(recipe.getServingCount());
-    Assertions.assertNotNull(recipe.getUpdatedAt());
-    Assertions.assertNotNull(recipe.getIngredients());
-  }
-
-  @Test
-  void shouldNotFetchRecipeByInvalidId() throws Exception {
-    String fetchedRecipe =
-        mockMvc
-            .perform(get("/api/recipes/100").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isNotFound())
-            .andDo(print())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
-    Assertions.assertNull(recipe.getName());
-    Assertions.assertNull(recipe.getIngredients());
-    Assertions.assertNull(recipe.getCreatedAt());
-    Assertions.assertNull(recipe.getUpdatedAt());
-  }
-
-  private String asJsonString(final Object obj) {
-    try {
-      return new ObjectMapper().writeValueAsString(obj);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    @BeforeAll
+    public static void setUp() {
+        faker = new Faker();
     }
-  }
+
+    @Test
+    void shouldFetchAllRecipes() throws Exception {
+        String fetchedRecipes = mockMvc.perform(get("/api/recipes").accept(MediaType.APPLICATION_JSON)).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<RecipeDto> recipes = List.of(objectMapper.readValue(fetchedRecipes, RecipeDto[].class));
+        Assertions.assertNotNull(recipes);
+        Assertions.assertEquals("Peanut Butter Pretzel Cookies", recipes.get(0).getName(), "names are not equal");
+        Assertions.assertEquals("Test", recipes.get(0).getInstruction(), "instructions are not equal");
+        Assertions.assertEquals("Vegetarian", recipes.get(0).getVariant(), "variants are not equal");
+        Assertions.assertEquals(4, recipes.get(0).getServingCount(), "serving count are not equal");
+        Assertions.assertNotNull(recipes.get(0).getCreatedAt());
+        Assertions.assertNotNull(recipes.get(0).getUpdatedAt());
+        Assertions.assertNotNull(recipes.get(0).getIngredients());
+    }
+
+    @Test
+    void shouldFetchRecipeById() throws Exception {
+        String fetchedRecipe = mockMvc.perform(get("/api/recipes/2").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
+        Assertions.assertNotNull(recipe);
+        Assertions.assertEquals("Green Beans with Toasted Butter Pecans", recipe.getName(), "names are not equal");
+        Assertions.assertNotNull(recipe.getCreatedAt());
+        Assertions.assertNotNull(recipe.getVariant());
+        Assertions.assertNotNull(recipe.getInstruction());
+        Assertions.assertNotNull(recipe.getServingCount());
+        Assertions.assertNotNull(recipe.getUpdatedAt());
+        Assertions.assertNotNull(recipe.getIngredients());
+    }
+
+    @Test
+    void shouldNotFetchRecipeByInvalidId() throws Exception {
+        String fetchedRecipe = mockMvc.perform(get("/api/recipes/100").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound()).andDo(print()).andReturn().getResponse().getContentAsString();
+        RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
+        Assertions.assertNull(recipe.getName());
+        Assertions.assertNull(recipe.getIngredients());
+        Assertions.assertNull(recipe.getCreatedAt());
+        Assertions.assertNull(recipe.getUpdatedAt());
+    }
+
+    @Test
+    void shouldFetchRecipesBySortedAndPaginatedWithDefaultValues() throws Exception {
+        String fetchedRecipes = mockMvc.perform(get("/api/recipes/page").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<RecipeDto> recipes = List.of(objectMapper.readValue(fetchedRecipes, RecipeDto[].class));
+        Assertions.assertNotNull(recipes);
+        Assertions.assertEquals("Peanut Butter Pretzel Cookies", recipes.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(recipes.get(0).getCreatedAt());
+        Assertions.assertNotNull(recipes.get(0).getUpdatedAt());
+        Assertions.assertNotNull(recipes.get(0).getIngredients());
+        Assertions.assertNotNull(recipes.get(0).getInstruction());
+        Assertions.assertNotNull(recipes.get(0).getVariant());
+        Assertions.assertNotNull(recipes.get(0).getServingCount());
+    }
+
+    @Test
+    void shouldFetchRecipesBySortedAndPaginatedWithSpecifiedValues() throws Exception {
+        String fetchedRecipes = mockMvc.perform(get("/api/recipes/page").accept(MediaType.APPLICATION_JSON).param("pageNo", "0").param("pageSize", "5").param("sortBy", "ASC").param("attribute", "id")).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<RecipeDto> recipes = List.of(objectMapper.readValue(fetchedRecipes, RecipeDto[].class));
+        Assertions.assertNotNull(recipes);
+        Assertions.assertEquals("Peanut Butter Pretzel Cookies", recipes.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(recipes.get(0).getCreatedAt());
+        Assertions.assertNotNull(recipes.get(0).getUpdatedAt());
+        Assertions.assertNotNull(recipes.get(0).getIngredients());
+        Assertions.assertNotNull(recipes.get(0).getVariant());
+        Assertions.assertNotNull(recipes.get(0).getInstruction());
+        Assertions.assertNotNull(recipes.get(0).getServingCount());
+    }
+
+    @Test
+    void shouldCreateRecipe() throws Exception {
+        IngredientDto ingredientDto = new IngredientDto();
+        ingredientDto.setName(faker.food().ingredient());
+        ingredientDto.setCreatedAt(LocalDateTime.now());
+        ingredientDto.setUpdatedAt(LocalDateTime.now());
+        Set<IngredientDto> ingredients = new HashSet<>(Collections.singleton(ingredientDto));
+        mockMvc.perform(post("/api/recipes").content(asJsonString(new RecipeDto(faker.food().dish(), "IT instruction", "IT variant", 5, LocalDateTime.now(), LocalDateTime.now(), ingredients))).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(print()).andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void shouldUpdateRecipe() throws Exception {
+        String fetchedRecipe = mockMvc.perform(put("/api/recipes/6").content(asJsonString(new RecipeDto(faker.food().dish(), "IT update instruction", "IT update variant", 7, LocalDateTime.now(), LocalDateTime.now(), new HashSet<>()))).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
+        Assertions.assertNotNull(recipe);
+        Assertions.assertNotNull(recipe.getCreatedAt());
+        Assertions.assertNotNull(recipe.getVariant());
+        Assertions.assertNotNull(recipe.getInstruction());
+        Assertions.assertNotNull(recipe.getServingCount());
+        Assertions.assertNotNull(recipe.getUpdatedAt());
+    }
+
+    @Test
+    void shouldPartiallyUpdateRecipe() throws Exception {
+        String fetchedRecipe = mockMvc.perform(patch("/api/recipes/5").content(asJsonString(new RecipeDto("Kumquat Cake", "Test", "NonVegetarian", 1, LocalDateTime.now(), LocalDateTime.now(), new HashSet<>()))).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        RecipeDto recipe = objectMapper.readValue(fetchedRecipe, RecipeDto.class);
+        Assertions.assertNotNull(recipe);
+        Assertions.assertNotNull(recipe.getCreatedAt());
+        Assertions.assertNotNull(recipe.getVariant());
+        Assertions.assertNotNull(recipe.getInstruction());
+        Assertions.assertNotNull(recipe.getServingCount());
+        Assertions.assertNotNull(recipe.getUpdatedAt());
+    }
+
+    @Test
+    void shouldRemoveRecipeById() throws Exception {
+        mockMvc.perform(delete("/api/recipes/7")).andExpect(status().isNoContent()).andDo(print()).andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void shouldSearchRecipesInSortedAndPaginatedWithSearchCriteria() throws Exception {
+        List<SearchCriteria> searchCriteriaList = new ArrayList<>();
+        SearchCriteria searchCriteria = new SearchCriteria(RequestKeyType.name.toString(), RequestOperationType.CN.toString(), "Peanut Butter", RequestDataOptionType.ALL.toString());
+        searchCriteriaList.add(searchCriteria);
+        String fetchedRecipes = mockMvc.perform(post("/api/recipes/search").content(asJsonString(new SearchRequest(searchCriteriaList, RequestDataOptionType.ALL.toString()))).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).param("pageNo", "0").param("pageSize", "10").param("sortBy", "ASC")).andExpect(status().isOk()).andDo(print()).andReturn().getResponse().getContentAsString();
+        List<RecipeDto> recipes = List.of(objectMapper.readValue(fetchedRecipes, RecipeDto[].class));
+        Assertions.assertNotNull(recipes);
+        Assertions.assertEquals("Peanut Butter Pretzel Cookies", recipes.get(0).getName(), "names are not equal");
+        Assertions.assertNotNull(recipes.get(0).getCreatedAt());
+        Assertions.assertNotNull(recipes.get(0).getUpdatedAt());
+        Assertions.assertNotNull(recipes.get(0).getIngredients());
+        Assertions.assertNotNull(recipes.get(0).getVariant());
+        Assertions.assertNotNull(recipes.get(0).getInstruction());
+        Assertions.assertNotNull(recipes.get(0).getServingCount());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
